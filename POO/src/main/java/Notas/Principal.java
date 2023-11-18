c58e05ca59109b8ca7368affcfd3b077164a8247
svelazquezj <svelasquezj@unal.edu.co>
2023-11-17 23:15:45 -0500

    Ejercicios 20% Actividad 5

---
+++ POO/src/main/java/Notas/Principal.java
@@ -0,0 +1,10 @@
+package Notas;
+
+public class Principal {
+    public static void main(String[] args){
+     VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal(); 
+     miVentanaPrincipal.setVisible(true);
+     
+     
+    }
+}
